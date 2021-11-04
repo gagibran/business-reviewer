803ea32d9091ab4a597fabbd261156947c67bd03
Gabriel Alvares Gibran <gagibran@gmail.com>
2021-11-04 18:23:50 -0300

    Added comments regarding the cancellation token

--- src/Application/Common/Interfaces/IApplicationDBContext.cs
+++ src/Application/Common/Interfaces/IApplicationDBContext.cs
@@ -9,7 +9,9 @@ namespace BusinessReviewer.Application.Common.Interfaces
     {
         DbSet<Review> Reviews { get; set; }
 
-        // This comes from Microsoft.EntityFrameworkCore.DbContext. Thus, it has to be specified here:
+        // This comes from Microsoft.EntityFrameworkCore.DbContext. Thus, it has to be specified here.
+        // Setting the cancellation token to default is the same as creating an empty object.
+        // More info: https://docs.microsoft.com/en-us/dotnet/api/system.threading.cancellationtoken.none?view=netstandard-2.0#remarks
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }

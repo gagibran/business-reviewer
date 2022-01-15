49e2e814d4c85f8d555c6495394fa8d1c7240bf3
Gabriel Alvares Gibran <gagibran@gmail.com>
2022-01-15 14:28:35 -0300

    Refactored App and removed MobX's store from it

--- src/client-app/src/components/App.tsx
+++ src/client-app/src/components/App.tsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import { v4 } from "uuid";
 import BusinessFormRefs from "../common/interfaces/businessFormRefs";
-import { store, StoreContext } from "../stores/store";
 import BusinessForm from "./BusinessForm";
 import ReviewForm from "./ReviewForm";
 import SideNav from "./SideNav";
@@ -21,21 +20,22 @@ const App = function () {
 
     return (
         <main>
-            {/*
-            Providing the store context to the whole app.
-            By doing that, every component can access the states
-            stored in the store.
-            */}
-            <StoreContext.Provider value={store}>
-                <SideNav
-                    userName="Joanna Johnson"
-                    userEmail="joanna.johnson@example.com"
-                    profilePictureSrc="https://images.unsplash.com/photo-1619895862022-09114b41f16f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                />
-                <ReviewForm reviewerId={v4()} businessId={v4()} />
-                <BusinessForm businessFormRefs={businessFormRefs} userId={v4()} />
-                <WorldMap businessFormRefs={businessFormRefs} />
-            </StoreContext.Provider>
+            <SideNav
+                userName="Joanna Johnson"
+                userEmail="joanna.johnson@example.com"
+                profilePictureSrc="https://images.unsplash.com/photo-1619895862022-09114b41f16f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            />
+            <ReviewForm
+                reviewerId={v4()}
+                businessId={v4()}
+            />
+            <BusinessForm
+                businessFormRefs={businessFormRefs}
+                userId={v4()}
+            />
+            <WorldMap
+                businessFormRefs={businessFormRefs}
+            />
         </main>
     );
 };
